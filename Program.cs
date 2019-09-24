3603367503a4fc4a75bbcb5efb763a762d16d139
hablee <46622299+hablee@users.noreply.github.com>
2019-09-24 17:41:55 +0800

    Add files via upload
    set window's appearance

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "我的应用程序";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("第一行文本");
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("第二行文本");
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("第三行文本");
+
+            Console.Read();
+        }
+    }
+}
